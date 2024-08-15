2d549dcc087543bc9f412bd9d5676d643010a3f8
Amy Ramadhan <amyramadhan99@gmail.com>
2024-08-15 15:51:22 +0700

    feat: add about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from './components/hero/hero'
+import About from './components/About'
 
 
 const App = () => {
   return (
     <>
      <Hero />
+     <About />
     </>
   )
 }
